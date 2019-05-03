e40b36eada67260852a5f2ccacc74caf66ca8d30
spectrometerHBH <spectrometer@sjtu.edu.cn>
2019-05-03 23:04:33 +0800

    [Codegen] oj test

--- src/Compiler/Optim/LoopInvariantCodeMotion.java
+++ src/Compiler/Optim/LoopInvariantCodeMotion.java
@@ -4,7 +4,7 @@ import Compiler.IR.IRRoot;
 
 //This pass performs loop invariant code motion, attempting to remove as much code from the body of a loop as possible.
 // It does this by either hoisting code into the preheader block, or by sinking code to the exit blocks if it is safe. ]
-// This pass also promotes must-aliased memory locations in the loop to live in registers, thus hoisting and sinking “invariant” loads and stores.
+// This pass also promotes must-aliased memory locations in the loop to live in registers, thus hoisting and sinking invariant loads and stores.
 //LLVM Pass
 //TODO
 
