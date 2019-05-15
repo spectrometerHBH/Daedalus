e538e85c3aa68fe80656bcfb6f0dbaf63fa1b3e7
spectrometerHBH <spectrometer@sjtu.edu.cn>
2019-05-15 15:47:52 +0800

    于是，终结的第五早已失去了意义。

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # Daedalus
 ACM Class Compiler project 
 
-By spectrometer
+Additionally, I implemented an interpreter for IR inherited from Lequn Chen(abcdabcd987) for debug use.
+
+## Feature
+- Dead code elimination on AST
+- Inline expansion
+- Control flow optimization
+  - Removing unreachable code
+  - Removing single branch basic block
+  - Converting target clear branch to jump
+  - Merge basic block
+- Data flow optimization based on static single assignment form
+  - Construction
+  - Extended local value numbering
+  - Aggressive dead code elimination
+  - Copy propagation
+  - Constant propagation
+  - Instruction combining, especially adressing mode combining
+  - Destruction
+- Graph coloring register allocator
+  - Iterated coalescing
+  - Spill heuristic based on loop level calculation
+
+- Small optimization
+  - Fast division and modular by compile time constants
+  - Memorization for trivial functions
--- src/Compiler/Main.java
+++ src/Compiler/Main.java
@@ -13,7 +13,8 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class Main {
     private static ProgramNode buildAST(InputStream in) throws Exception {
@@ -28,28 +29,6 @@ public class Main {
     public static void main(String... args) throws Exception {
         //for program to be compiled
         InputStream in = new FileInputStream("test.txt");
-
-        //compile config
-        boolean test_ir = false;
-        boolean test_nasm = true;
-        boolean DEBUG_IR = false;
-
-        //for text-ir output
-        PrintStream ir_out_raw = new PrintStream("ir_raw.ll");
-        PrintStream ir_out_after_inline = new PrintStream("ir_after_inlining.ll");
-        PrintStream ir_out_after_memorize = new PrintStream("ir_after_memorization.ll");
-        PrintStream ir_out_after_globalVariableResolve = new PrintStream("ir_after_gvResolve.ll");
-        PrintStream ir_out_afterSSAConstruction = new PrintStream("ir_out_ssa_construct.ll");
-        PrintStream ir_out_afterOptimization = new PrintStream("ir_out_after_optim.ll");
-        PrintStream ir_out_afterX86Transform = new PrintStream("ir_out_after_X86.ll");
-        PrintStream ir_codegen_without_color = new PrintStream("ir_out_after_codegen_no_color.ll");
-        PrintStream ir_codegen = test_ir ? new PrintStream(System.out) : new PrintStream("ir_out_after_codegen.ll");
-        PrintStream nasm = test_nasm ? new PrintStream(System.out) : new PrintStream("out.asm");
-
-        //for IR interpreter test use
-        FileInputStream ir_test_in = new FileInputStream("ir_raw.ll");
-        DataInputStream ir_data_in = new DataInputStream(System.in);
-        PrintStream ir_data_out = new PrintStream(new FileOutputStream("ir_test_out.txt"));
 
         try {
             //Syntax Analysis
@@ -72,19 +51,14 @@ public class Main {
             IRBuilder irBuilder = new IRBuilder(globalScope);
             irBuilder.visit(ast);
             IRRoot irRoot = irBuilder.getIrRoot();
-            if (DEBUG_IR) new IRPrinter(ir_out_raw).visit(irRoot);
             new FunctionInliner(irRoot).run();
-            if (DEBUG_IR) new IRPrinter(ir_out_after_inline).visit(irRoot);
             new MemorizationSeeker(irRoot).run();
-            if (DEBUG_IR) new IRPrinter(ir_out_after_memorize).visit(irRoot);
             new GlobalVariableResolver(irRoot).run();
-            if (DEBUG_IR) new IRPrinter(ir_out_after_globalVariableResolve).visit(irRoot);
 
             //LIR Optimization based on SSA
             Optimizer optimizer = new Optimizer(irRoot);
             optimizer.CFGSimplification();
             optimizer.SSAConstruction();
-            if (DEBUG_IR) new IRPrinter(ir_out_afterSSAConstruction).visit(irRoot);
             for (boolean changed = true; changed; ) {
                 changed = optimizer.CommonSubexpressionElimination();
                 changed |= optimizer.ConstantAndCopyPropagation();
@@ -93,22 +67,17 @@ public class Main {
                 changed |= optimizer.CFGSimplification();
             }
             optimizer.InstructionCombination();
-            if (DEBUG_IR) new IRPrinter(ir_out_afterOptimization).visit(irRoot);
             optimizer.SSADestruction();
             optimizer.CFGSimplification(true);
 
             //Codegen
             optimizer.ArithmeticTransformation();
             new X86ConstraintResolver(irRoot).run();
-            if (DEBUG_IR) new IRPrinter(ir_out_afterX86Transform).visit(irRoot);
             optimizer.SpillPriorityCalculation();
             new RegisterAllocator(irRoot).run();
             optimizer.CFGSimplification(true);
-            if (DEBUG_IR) new IRPrinter(ir_codegen_without_color).visit(irRoot);
-            if (DEBUG_IR) new IRPrinter(ir_codegen, true).visit(irRoot);
-            new X86CodeEmitter(irRoot, nasm).run();
+            new X86CodeEmitter(irRoot, System.out).run();
 
-            //new IRInterpreter_codegen(ir_test_in, false, ir_data_in, ir_data_out).run();
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getMessage());
